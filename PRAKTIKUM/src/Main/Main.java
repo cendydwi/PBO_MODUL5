07b0fc560ef81850465c0bb98e28aefacf87a277
cendydwie <cendydwie47@gmail.com>
2019-12-01 22:00:44 +0700

    Praktikum

--- PRAKTIKUM/src/Main/Main.java
+++ PRAKTIKUM/src/Main/Main.java
@@ -1,12 +1,49 @@
 package Main;
 
-import Operasi.Proses;
+import Operasi.*;
 import Data.Data;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Abstraksi{
     static Data data = new Data();
-    static Proses proses = new Proses();
+    
+    @Override
+    public void seq_search(int cari){
+        int found = -1;
+        for(int i = 0; i < data.getLength(); i++){
+            if(data.getData(i) == cari){
+                System.out.println("\nData ada di index-"+i+"\n");
+                found = 1;
+                break;
+            }else{
+                found = 0;
+            }
+        }
+        if(found == 0){
+            System.out.println("\nData Tidak Ada\n");
+        }
+    }
+    
+    @Override
+    public void bin_search(int cari){
+        int awal = 0, akhir= data.getLength()-1, found=0;
+        for(int i = 0; i < data.getLength(); i++){
+            int tengah = (akhir-awal)/2;
+            if(data.getData(i) == cari){
+                found = i;
+            }else if(data.getData(tengah) > cari){
+                akhir = tengah - 1;
+            }else{
+                awal = tengah+1;
+            }
+        }
+        
+        if(found == 0){
+            System.out.println("\nData Tidak Ada\n");
+        }else{
+            System.out.println("\nData ada di index-"+found+"\n");
+        }
+    }
     
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -20,22 +57,37 @@ public class Main {
                     try {
                         System.out.print("Masukkan Angka: ");
                         int angka = in.nextInt();
+                        System.out.println("");
                         data.setData(index, angka);
                         index++;
                     } catch (ArrayIndexOutOfBoundsException e) {
-                        System.out.println("DATA PENUH!!!");
+                        System.out.println("DATA PENUH!!!\n");
                     }
                 break;
                     
                 case 2:
-                    System.out.println("DATA");
-                    for (int i = 0; i < data.getLength(); i++) {
-                        System.out.println(proses.getAngka(i));
+                    System.out.println("\nDATA");
+                    for (int i = 0; i < index; i++) {
+                        System.out.println(data.getData(i));
                     }
+                    System.out.println("");
                 break;
                     
                 case 3:
-                    proses.getAngka(1);
+                    System.out.print("Masukkan Angka ");
+                    cari = in.nextInt();
+                    System.out.println("");
+                    System.out.print("Pilihan Pencarian\n1.Sequential Search\n2.Binary Search\nPilih: ");
+                    pil_cari = in.nextInt();
+                    switch(pil_cari){
+                        case 1:
+                            new Main().seq_search(cari);
+                            break;
+                            
+                        case 2:
+                            new Main().bin_search(cari);
+                            break;
+                    }
                 break;    
             }
         }while(pilih!=0);
